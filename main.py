0530c2de3e439e322379188bc32e7c09f034c38d
jeromesjj <jerome.seahjj@gmail.com>
2024-07-20 23:53:41 +0800

    final commit
    Simple python snake game completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -10,6 +12,8 @@ screen.tracer(0)
 
 # Should look like this
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,5 +26,23 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 \
+        or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+    
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
